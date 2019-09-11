101a218d30c7c7e84e5dcc02680435afde7a6a79
Elcee <liamcoughlanlife@gmail.com>
2019-09-11 12:56:53 -0400

    first commit after cloning boiler plate, updated server and created base app

--- server.js
+++ server.js
@@ -1,19 +1,102 @@
-let express = require('express')
-let app = express()
-let reloadMagic = require('./reload-magic.js')
+let express = require("express");
+let app = express();
+let multer = require("multer");
+let upload = multer();
+let reloadMagic = require("./reload-magic.js");
+let MongoClient = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectID;
+let dbo;
+let result;
+let url = require("./mongoUrl.js"); //your MongoDB url goes here
+MongoClient.connect(url(), { useNewUrlParser: true }, (err, db) => {
+  dbo = db.db("DiscGolfData");
+});
+const cheerio = require("cheerio");
+const request = require("request");
 
-reloadMagic(app)
+reloadMagic(app);
 
-app.use('/', express.static('build')); // Needed for the HTML and JS files
-app.use('/', express.static('public')); // Needed for local assets
+app.use("/", express.static("build"));
+app.use("/", express.static("public"));
 
-// Your endpoints go after this line
+app.post("/loadEventDetails", upload.none(), (req, resp) => {
+  let id = req.body.id;
+  request(
+    { method: "GET", url: "https://www.pdga.com/tour/event/" + id },
+    (err, res, body) => {
+      let $ = cheerio.load(body);
 
-// Your endpoints go before this line
+      let MPOPlayers = [];
+      let MPORatings = [];
+      let FPOPlayers = [];
+      let FPORatings = [];
+      let title = $("#page-title").text();
 
-app.all('/*', (req, res, next) => { // needed for react router
-    res.sendFile(__dirname + '/build/index.html');
-})
+      $("#tournament-stats-0")
+        .find("a")
+        .each(function(i, e) {
+          MPOPlayers[i] = $(this).text();
+        });
 
+      $("#tournament-stats-0")
+        .find(".player-rating")
+        .each(function(i, e) {
+          MPORatings[i] = $(this).text();
+        });
 
-app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+      $("#tournament-stats-1")
+        .find("a")
+        .each(function(i, e) {
+          FPOPlayers[i] = $(this).text();
+        });
+
+      $("#tournament-stats-1")
+        .find(".player-rating")
+        .each(function(i, e) {
+          FPORatings[i] = $(this).text();
+        });
+
+      MPORatings = MPORatings.slice(1);
+      FPORatings = FPORatings.slice(1);
+
+      let amalgamatedMPO = [];
+      let amalgamatedFPO = [];
+
+      for (players = 0; players < MPOPlayers.length; players++) {
+        let object = {
+          playerName: MPOPlayers[players],
+          rating: MPORatings[players]
+        };
+        amalgamatedMPO.push(object);
+      }
+
+      for (players = 0; players < FPOPlayers.length; players++) {
+        let object = {
+          playerName: FPOPlayers[players],
+          rating: FPORatings[players]
+        };
+        amalgamatedFPO.push(object);
+      }
+
+      dbo
+        .collection("Tournaments")
+        .insertOne({ [title]: { MPO: amalgamatedMPO, FPO: amalgamatedFPO } });
+
+      resp.send(
+        JSON.stringify({
+          success: true,
+          MPO: amalgamatedMPO,
+          FPO: amalgamatedFPO
+        })
+      );
+    }
+  );
+});
+
+app.all("/*", (req, res, next) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server running on port 4000");
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
-    render = () => {
-        return "Hello world!"
+  constructor(props) {
+    super(props);
+    this.state = {
+      idInput: ""
+    };
+  }
+  onSubmitHandler = async event => {
+    event.preventDefault();
+    let data = new FormData();
+    data.append("id", this.state.idInput);
+    let response = await fetch("/loadEventDetails", {
+      method: "POST",
+      body: data
+    });
+    let responseBody = await response.text();
+    let body = JSON.parse(responseBody);
+    if (body.success) {
+      alert("Event Added");
+      this.setState({ idInput: "" });
+    } else {
+      alert("Maybe a problem");
+      this.setState({ idInput: "" });
     }
+  };
+
+  onChangeHandler = event => {
+    this.setState({ idInput: event.target.value });
+  };
+
+  render = () => {
+    return (
+      <div>
+        <form onSubmit={this.onSubmitHandler}>
+          <div>Enter PDGA event id number</div>
+          <input
+            type="text"
+            onChange={this.onChangeHandler}
+            value={this.state.idInput}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  };
 }
 
-
-export default App
+export default App;
